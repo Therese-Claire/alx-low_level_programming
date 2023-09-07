d5f26cc3c29947d40b5a4b3d28a81ea915485662
Therese-Claire <awonguagabi@gmail.com>
2023-09-07 15:39:17 -0700

    printing output without puts abd printf

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, message, sizeof(message) - 1);
+
+	return (1);
+}

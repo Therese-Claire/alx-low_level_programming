b63b2514f59015fdf5bf119f2bbb0d31547a279f
Therese-Claire <awonguagabi@gmail.com>
2023-09-08 14:52:11 -0700

    printing 2digit numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,12 +9,13 @@ int main(void)
 {
 	int num1, num2;
 
-	for (num1 = 0; num1 < 9; num1++)
+	for (num2 = num1 + 1; num1 < 9; num1++)
 	{
 		for (num2 = 0; num2 < 10; num2++)
 		{
 			putchar('0' + num1);
 			putchar('0' + num2);
+
 			if (num1 < 8 || num2 < 9)
 			{
 				putchar(',');

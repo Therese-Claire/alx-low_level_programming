2e8a9134bfdf7cea0db46a28b31770ce00f28209
Therese-Claire <awonguagabi@gmail.com>
2023-09-08 12:13:13 -0700

    printing lettrs z-a using putchar

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	ch = 'z';
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		ch = ch - 1;
+	}
+
+	putchar('\n');
+
+	return (0);
+}

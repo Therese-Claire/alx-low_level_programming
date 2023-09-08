eee1ddbb902fa718eeffe2f520e4f73353955b35
Therese-Claire <awonguagabi@gmail.com>
2023-09-08 16:21:33 -0700

    printing two two digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (int num1 = 0; num1 < 100; num1++)
+	{
+		int tens1 = num1 / 10;
+		int ones1 = num1 % 10;
+
+		for (int num2 = num1; num2 < 100; num2++)
+		{
+			int tens2 = num2 / 10;
+			int ones2 = num2 % 10;
+
+			if (num1 <= num2)
+			{
+				putchar(tens1 / 10 + '0');
+				putchar(tens1 % 10 + '0');
+				putchar(' ');
+				putchar(ones1 / 10 + '0');
+				putchar(ones1 % 10 + '0');
+				putchar(',');
+				putchar(' ');
+				putchar(tens2 / 10 + '0');
+				putchar(tens2 % 10 + '0');
+				putchar(' ');
+				putchar(ones2 / 10 + '0');
+				putchar(ones2 % 10 + '0');
+
+				if (num1 != 99 || num2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}

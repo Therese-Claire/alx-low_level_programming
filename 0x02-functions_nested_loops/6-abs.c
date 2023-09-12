66489626f8505798eae992afc59e15bee3f41e30
Therese-Claire <awonguagabi@gmail.com>
2023-09-12 16:51:48 -0700

    code to print absolute values

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer.
+ *@int : data type of the absolute value
+ *
+ * Return: The absolute value of the input integer.
+ */
+int _abs(int)
+{
+	if (n < 0)
+		return (-n);
+	else
+		return (n);
+}

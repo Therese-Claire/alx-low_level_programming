c0a050dfc3b3f7823005dd9ac1db49e55e88d513
Therese-Claire <awonguagabi@gmail.com>
2023-09-12 17:01:25 -0700

    code to print absolute values

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,11 +2,11 @@
 
 /**
  * _abs - computes the absolute value of an integer.
- *@int : data type of the absolute value
+ *@n : function parameter to store the value input
  *
  * Return: The absolute value of the input integer.
  */
-int _abs(int)
+int _abs(int n)
 {
 	if (n < 0)
 		return (-n);

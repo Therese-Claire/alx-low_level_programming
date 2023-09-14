23366e10155cb4a7d8734bb275d0cea7773d13a0
Therese-Claire <awonguagabi@gmail.com>
2023-09-14 02:06:59 -0700

    printing 9 times table using _putchar

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,27 +5,33 @@
  */
 void times_table(void)
 {
-	int i, j;
+	int a, b, c, d, e;
 
-	for (i = 0; i <= 9; i++)
+	for (a = 0; a <= 9; a++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (b = 0; b <= 9; b++)
 		{
-			int product;
-
-			product = i * j;
-
-			if (product < 10)
+			c = a * b;
+			if (c > 9)
 			{
-
-				_putchar('0' + product / 10);
-				_putchar('0' + product % 10);
+			d = c % 10;
+			e = (c - d) / 10;
+			_putchar(44);
+			_putchar(32);
+			_putchar(e + '0');
+			_putchar(d + '0');
 			}
-
-			_putchar(' ');
-
+			else
+			{
+			if (b != 0)
+			{
+			_putchar(44);
+			_putchar(32);
+			_putchar(32);
+			}
+			_putchar(c + '0');
+			}
 		}
-
 		_putchar('\n');
 	}
 }

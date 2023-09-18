1f166b92f02471e37dd433c017b6781fc65c709e
Therese-Claire <awonguagabi@gmail.com>
2023-09-18 15:54:34 -0700

    printing digits of an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_array - function that prints n elements of an array of integers.
+ * @a: pointer that points to integer
+ * @n: numbers of elements in the array
+ *
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+	}
+
+	printf("\n");
+}

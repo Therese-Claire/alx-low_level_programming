304434c0e57ffc04b1035b39b1d358e0d2644e2d
Therese-Claire <awonguagabi@gmail.com>
2023-09-18 15:07:57 -0700

    printing seconf half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * puts_half - function prints half of string
+ * @str: points to the string.
+ *
+ */
+void puts_half(char *str)
+{
+	int i;
+	int len = 0;
+	int start;
+
+	while (str[len] != 0)
+	{
+		len++;
+	}
+	if (len % 2 == 0)
+	{
+		start = len / 2;
+	}
+	else
+	{
+		start len = (len - 1) / 2;
+	}
+	for (i = start; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}

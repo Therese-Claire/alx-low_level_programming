7ba4cdb56b812e2a5da01945ef4c3105e40f30d2
Therese-Claire <awonguagabi@gmail.com>
2023-09-21 04:59:15 -0700

    function to copy string from src to dest

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,11 +13,12 @@ char *_strncpy(char *dest, char *src, int n)
 	char *ptr = dest;
 	int i = 0;
 
-	while (i < n && src[i] != '\0')
+	while (src[i] != '\0' && n > 0;)
 	{
 		dest[i] = src[i];
 
 		i++;
+		n--;
 	}
 
 	dest[i] = '\0';

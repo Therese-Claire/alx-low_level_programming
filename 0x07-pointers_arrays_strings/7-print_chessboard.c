5c44c48fc40dc2676b32aa4176777460d9cab825
Therese-Claire <awonguagabi@gmail.com>
2023-09-26 16:15:40 -0700

    prints a chestboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Fimction prints a chest board
+ * @a : points to the array of strings.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}

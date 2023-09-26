ac540fb6924f5848a102ee9cce3215be08781134
Therese-Claire <awonguagabi@gmail.com>
2023-09-26 07:41:20 -0700

    copying n bytes from src to dest mrmory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_memcpy - This function copies n bytes from
+ * src memory area to dest memory area.
+ * @dest: points to dest memory area.
+ * @src: points to src memory area.
+ * @n: num of bytes to copy from memory area.
+ *
+ * Return: Pointer to dest.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *ptr = dest;
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*ptr = *src;
+		ptr++;
+		src++;
+	}
+
+	return (dest);
+}

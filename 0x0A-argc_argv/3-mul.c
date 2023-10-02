a3f6cb247b19ae9ce65fbee476442d450098c5ee
Therese-Claire <awonguagabi@gmail.com>
2023-10-02 14:18:52 -0700

    multiplying 3 args

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - The main function
+ * @argc : argument count
+ * @argv : argument vector which points to the array of
+ * string of arguments.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int res;
+
+	if (argc - 1 != 2)
+	{
+		printf("Error\n");
+
+		return(1);
+	}
+	else
+	{
+		res = atoi(argv[1]) * atoi(argv[2]);
+
+		printf("%d\n", res);
+	}
+	return (0);
+}

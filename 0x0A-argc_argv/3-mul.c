da9e5c19884079b29bd30e9f0c5420ee6ad6b028
Therese-Claire <awonguagabi@gmail.com>
2023-10-02 14:54:27 -0700

    multiplying 3 args

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,21 +11,14 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2;
-	int res;
-
 	if (argc < 3)
 	{
 		printf("Error\n");
 
 		return(1);
 	}
-
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-
-	res = num1 * num2;
-	printf("%d\n", res);
+	else
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 
 	return (0);
 }

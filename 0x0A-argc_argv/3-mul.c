8df087bbe41d1a8cecad1cb198c9c68e48605371
Therese-Claire <awonguagabi@gmail.com>
2023-10-02 15:08:01 -0700

    multiplying 2 int arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,11 +1,11 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - The main function
+ *
  * @argc : argument count
- * @argv : argument vector which points to the array of
- * string of arguments.
+ * @argv : argument vector which points to the array of string of arguments.
  *
  * Return: Always 0.
  */
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 	{
 		printf("Error\n");
 
-		return(1);
+		return (1);
 	}
 	else
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));

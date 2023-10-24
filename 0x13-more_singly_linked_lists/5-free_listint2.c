d0480be031591ff6a077b31e9a57c12dfe5975e0
Therese-Claire <awonguagabi@gmail.com>
2023-10-24 09:46:35 -0700

    function to freee linked list and sets head to null

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,7 +8,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *current_node;
 
-	while (*head != NULL)
+	while ((current_node = *head) != NULL)
 	{
 		current_node = *head;
 		*head = (*head)->next;

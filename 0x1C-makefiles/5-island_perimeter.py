82f6195a4cc49ccc0d4d78da1578e81aeb2c5dcc
Therese-Claire <awonguagabi@gmail.com>
2024-02-21 14:52:48 -0800

    5. Island Perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -13,19 +13,19 @@ def island_perimeter(grid):
 
     """
 
+    size = 0
     rows = len(grid)
     cols = len(grid[0])
-
     perimeter = 0
 
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
-                perimeter += 4
+                size += 1
 
                 if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2
+                    perimeter += 1
                 if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2
+                    perimeter += 1
 
-    return perimeter
+    return size * 4 - perimeter * 2
